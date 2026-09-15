85ae411807aea8aa779147ea35168f0c1281dc4f
Jhed Sarmiento <chryslrosrmnt@gmail.com>
2026-09-15 15:28:37 +0800

    change the apk file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
-  APK_DOWNLOAD_URL: '/cpu-scheduler.apk',
+  APK_DOWNLOAD_URL: '/OST.apk',
   APP_PREVIEW_IMAGE: '/ss.jpg',
   GITHUB_RELEASE_URL: 'https://github.com/chryslerx7/Cpu-Scheduler/releases/tag/v1.0.1',
 };
